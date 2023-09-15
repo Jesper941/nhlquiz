b7e6b0cc7db56257e43dc0e0af487ed148ecfe2b
Jesper941 <>
2023-09-15 19:12:19 +0000

    Add final score area and re-arrange id's to work better.

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -4,14 +4,17 @@ const startButton = document.getElementById("start-btn");
 const introArea = document.getElementById("intro-area");
 const usernameArea = document.getElementById("username-area");
 const quizArea = document.getElementById("quiz-area");
+const countArea = document.getElementById("count-area");
+const answerButtons = document.getElementById("answer-buttons");
 
-
+/*Listener for the start button*/
 startButton.addEventListener("click", (startQuiz));
 
+/*Functions that starts once the start button has been clicked*/
 function startQuiz() {
     startButton.style.display = "none";
     introArea.style.display = "none";
     usernameArea.style.display = "none";
     quizArea.style.display = "block";
-
+    countArea.style.display = "block";
 }
